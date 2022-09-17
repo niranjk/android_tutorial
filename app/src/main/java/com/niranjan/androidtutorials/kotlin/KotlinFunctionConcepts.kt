5d01cd431140db33e43c3fa0aa499188c7d89f81
Niranjan Khatri <niranjan.khatri@nttdata.com>
2022-09-17 23:59:58 +0200

    kotlin Basics - function concepts

---
+++ app/src/main/java/com/niranjan/androidtutorials/kotlin/KotlinFunctionConcepts.kt
@@ -0,0 +1,54 @@
+package com.niranjan.androidtutorials.kotlin
+
+class KotlinFunctionConcepts {
+
+    fun returningUnitFunc() : Unit{
+        println("Hi there! I return Unit.")
+    }
+
+    // every FUNCTION bloc performs a specific tasks
+    fun functionBloc(
+        // arguments passed
+        defaultValue : String = "Default", // Default arguments
+        requiredParams : String // Required parameters
+    ) : Boolean // Return Type
+    {
+        println("$defaultValue $requiredParams")
+        return true
+    }
+
+    // LAMBDA is an expression that makes a function that has no name
+    val lambdaFunctionValue: (Int) -> Int =
+        { level: Int -> // Parameters and Type
+            level / 2  // executed code
+        }
+
+    // HIGHER ORDER FUNCTIONS
+    // function that takes another function as arguments or return a function
+    fun higherOrderFunction(
+        msg: String,
+        transform: (String) -> String
+    ): String {
+        return transform(msg)
+    }
+
+    // Usecase
+    val myTransform: (String) -> String = { input ->
+        input.reversed()
+    }
+
+    fun namedFunctionNotLambda(input: String): String = input.drop(2)
+
+    fun usingHighOrderFunction(){
+        val transformMessage = higherOrderFunction(
+            msg = "This is my message i want to transform.",
+            myTransform
+        )
+        val tranformUsingReference = higherOrderFunction(
+            msg = "This message i want to drop",
+            ::namedFunctionNotLambda
+        )
+        println(transformMessage)
+        println(tranformUsingReference)
+    }
+}
