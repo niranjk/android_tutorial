4b7e0d280893b611dd25acba4942c4fe7c1c66fb
Niranjan Khatri <niranjan.khatri@nttdata.com>
2022-09-24 14:05:31 +0200

    kotlin - rollback compile and target sdk to 32 and added accessibility attributes for the views

--- app/src/test/java/com/niranjan/androidtutorials/ExampleUnitTest.kt
+++ app/src/test/java/com/niranjan/androidtutorials/ExampleUnitTest.kt
@@ -8,6 +8,8 @@ import org.junit.Assert.*
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
+ * See [https://developer.android.com/training/testing] for Testing Android App
+ * See [https://mockk.io] for mocking in Kotlin
  */
 class ExampleUnitTest {
     @Test
